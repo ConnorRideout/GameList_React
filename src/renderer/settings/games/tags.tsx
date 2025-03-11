22933da24f2fbe7ed5369cd232761d64a27d3719
Connor Rideout <connorrideout13@gmail.com>
2025-03-11 17:22:29 -0400

    added onChange to `Tags`s controlled inputs

--- src/renderer/settings/games/tags.tsx
+++ src/renderer/settings/games/tags.tsx
@@ -12,24 +12,39 @@ import { Props } from './games'
 export default function Tags({formData, setFormData}: Props) {
   const {tags} = formData
 
-  const handleChange = () => {}
+  const handleRemoveTag = () => {
+
+  }
+
+  const handleAddTag = () => {
+
+  }
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>, idx: number) => {
+    const { value } = evt.target
+    const oldTags = [...formData.tags]
+    oldTags[idx] = value
+    setFormData(prevValue => ({...prevValue, tags: oldTags}))
+  }
 
   return (
     <fieldset className='vertical-container scrollable grid-column-2'>
       <legend>TAGS</legend>
 
-      {tags.map(tag => (
-        <div className='horizontal-container align-center'>
+      {tags.map((tag, idx) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={`tag-${idx}`} className='horizontal-container align-center'>
           <button
             type='button'
             className='svg-button'
+            onClick={handleRemoveTag}
           >
             <MinusSvg size={17} />
           </button>
           <input
             type="text"
             value={tag}
-            onChange={handleChange}
+            onChange={(evt) => handleChange(evt, idx)}
           />
         </div>
       ))}
@@ -37,6 +52,7 @@ export default function Tags({formData, setFormData}: Props) {
         type='button'
         className='svg-button'
         style={{marginTop: '4px'}}
+        onClick={handleAddTag}
       >
         <PlusSvg size={17} />
       </button>
