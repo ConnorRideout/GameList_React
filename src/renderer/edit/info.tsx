a161f5d264bbeb2a96315693e88e87739f36033c
Connor Rideout <connorrideout13@gmail.com>
2025-08-11 17:38:48 -0400

    re-validate the form after auto-filling from the websites

--- src/renderer/edit/info.tsx
+++ src/renderer/edit/info.tsx
@@ -1,6 +1,4 @@
 // STRETCH: image editor/selector if image isn't 16:9
-// TODO: if image is changed, attempt to delete the old image so it's not still sitting in the fol
-// FIXME: image handling with gifs seems broken when editing. Make sure the right field is being edited
 import React, {ChangeEvent} from "react"
 import { useDispatch, useSelector } from "react-redux"
 
@@ -38,9 +36,16 @@ export interface Props {
   updatePickerDefaults: (newDefaults: {[type: string]: string | string[] | StringMap}) => void,
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
   ignoreUpdatedVersion: boolean,
-  setIgnoreUpdatedVersion: React.Dispatch<React.SetStateAction<boolean>>
+  setIgnoreUpdatedVersion: React.Dispatch<React.SetStateAction<boolean>>,
+  validateAll: (curFormData: Pick<GameEntry, "url" | "path" | "title" | "version" | "description"> & {
+    image: string;
+    program_path: {
+        id: number;
+        paths: [string, string];
+    }[];
+}) => void
 }
-export default function Info({handleFormChange, formData, setFormData, updatePickerDefaults, setIsLoading, ignoreUpdatedVersion, setIgnoreUpdatedVersion}: Props) {
+export default function Info({handleFormChange, formData, setFormData, updatePickerDefaults, setIsLoading, ignoreUpdatedVersion, setIgnoreUpdatedVersion, validateAll}: Props) {
   const dispatch = useDispatch()
   const settings = useSelector((state: RootState) => state.data.settings)
   const [autoFillFromWebsite] = useAutofillFromWebsiteMutation()
@@ -58,31 +63,31 @@ export default function Info({handleFormChange, formData, setFormData, updatePic
     }
   }
 
-  const handleAutoExeSearch = () => {
+  const handleAutoExeSearch = async () => {
     const existing_paths = formData.program_path.map(({paths: [,prog_pth]}) => prog_pth).filter(p => p.trim())
-    getExecutables({top_path: formData.path, existing_paths}).unwrap()
-      .then(({filepaths}: {filepaths: string[]}) => {
-        const program_path: {id: number, paths: [string, string]}[] = filepaths.map((pth, id) => {
-          const basepath = pth.replace(/\.[^.]*?$/, '')
-          const split_path = basepath.replaceAll(/(?<=[a-z])(?=[A-Z])|_/g, ' ')
-          return {id, paths: [toTitleCase(split_path).trim(), pth]}
-        })
-        setFormData(prevVal => ({...prevVal, program_path}))
+    let program_path: {id: number, paths: [string, string]}[] = []
+    try {
+      const {filepaths}: {filepaths: string[]} = await getExecutables({top_path: formData.path, existing_paths}).unwrap()
+      program_path = filepaths.map((pth, id) => {
+        const basepath = pth.replace(/\.[^.]*?$/, '')
+        const split_path = basepath.replaceAll(/(?<=[a-z])(?=[A-Z])|_/g, ' ')
+        return {id, paths: [toTitleCase(split_path).trim(), pth]}
       })
-      .catch(err => {
-        console.error(err)
-      })
+      setFormData(prevVal => ({...prevVal, program_path}))
+    } catch (err) {
+      console.error(err)
+    }
+    return program_path
   }
 
   const handleAutoFill = async () => {
     // TODO: when updating, show differences in tags/categories/etc and allow user to pick which ones to keep/change
-    // TODO: move protagonist to categories, and make a selector for it. Have the database be ordered by preference? IDK how to have 'multiple' be an option
     const { url } = formData
     const { website_id } = settings.site_scrapers.find(scraper => url.includes(scraper.base_url))!
     setIsLoading(true)
+    const newEditFormData: StringMap = {}
     try {
       const scraper_result = await autoFillFromWebsite({website_id, url}).unwrap()
-      const newEditFormData: StringMap = {}
       const newPickerFormData: {[type: string]: string | string[] | StringMap} = {}
       scraper_result.forEach(({type, parsed}) => {
         if (['title', 'description', 'version'].includes(type)) {
@@ -108,7 +113,8 @@ export default function Info({handleFormChange, formData, setFormData, updatePic
       dispatch(setError(error.message))
     }
     setIsLoading(false)
-    handleAutoExeSearch()
+    const program_path = await handleAutoExeSearch()
+    validateAll({...formData, ...newEditFormData, program_path})
   }
 
   const toggleIgnoreVersion = () => {
@@ -206,12 +212,12 @@ export default function Info({handleFormChange, formData, setFormData, updatePic
           checked={ignoreUpdatedVersion}
           onChange={toggleIgnoreVersion}
         />
-        Ignore
+        <i>Don&apos;t<br/>update</i>
       </label>
       <Tooltip
         anchorSelect="#versionIgnoreCheckbox"
       >
-        If this is selected, the &quot;updated at&quot; timestamp won&apos;t be changed, even if the version changes
+        If this is checked, the &quot;updated at&quot; timestamp won&apos;t be modified, even if the version changes
       </Tooltip>
 
       <span className="grid-column-1 grid-row-5 align-start">Program Path(s):</span>
